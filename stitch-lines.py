59a75310e8c6a889cb00fa465afbc31aa0ffbbb8
Carlos Rodrigues <carlosnsr@users.noreply.github.com>
2026-02-17 20:01:54 -0700

    Now reads in the headers

--- stitch-lines.py
+++ stitch-lines.py
@@ -2,33 +2,56 @@ import csv
 import re
 import sys
 
-def stitch_lines(input_file):
-    # db
+def make_db_row(type_):
+    return dict(
+        metadata=dict(type_=type_, status="incomplete"),
         # metadata
-            # type: Living, etc.
+            # type_: Living, etc.
             # status: complete, incomplete
+        header=dict(fields=[], status="incomplete"),
         # header
             # status: complete, incomplete
-            # fields:
+            # fields: []
                 # expect X fields
                 # if < X fields
                     # is incomplete
                     # if find a header later on, add it to the headers
+        items=dict(rows=[], status="incomplete")
         # items
             # status: complete, incomplete
-            # fields:
-                # expect X columns
-                # if < X columns
+            # rows: []
+                # expect X fields in each row
+                # if < X fields
                     # is incomplete
                     # if find an item field later on, add it to the items
+    )
+
+def stitch_lines(input_file):
     with open(input_file, 'r') as f:
         lines = [line.strip() for line in f if line.strip()]
 
-    # run through collecting headers
+    db = []
+    # run through once collecting headers
     i = 1 # The first line is the beginning of a headers
     while i < len(lines):
         line = lines[i]
-        print(f"{i}: SKIPPED: {line}")
+
+        if line.startswith("QUANTITY"):
+            # found a header line
+            type_ = lines[i - 1]
+            row = make_db_row(type_)
+
+            # all the possible headers
+            pattern = r"QUANTITY|UNIT|TAX|RCV|AGE/LIFE|COND\.|DEP %|DEPREC\.|ACV"
+            other_headers = re.findall(pattern, lines[i])
+            headers = [type_] + other_headers
+            row["header"]["fields"] = headers
+            if len(headers) == 10:
+                row["header"]["status"] = "complete"
+
+            print(f"{i}: HEADERS: {headers}")
+        else:
+            print(f"{i}: SKIPPED: {line}")
         i += 1
 
 if __name__ == "__main__":
